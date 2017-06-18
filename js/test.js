1661e43d5998f73cd72dac22e521138829092a22
Nazar Hmyz <hmyzn07@gmail.com>
2017-06-18 14:44:48 +0300

    My first commit

---
+++ js/test.js
@@ -0,0 +1,28 @@
+/*
+var answer1 = prompt('Який ваш ріст. Скільки метрів?');
+var answer2 = prompt('Скільки сантиметрів?')
+var ans1 = Number(answer1);
+var ans2 = Number(answer2);
+
+function height(a, b) {
+    var convert = a * 100;
+    var sum = convert + b;
+    console.log(sum + ' см');
+    return sum;
+};
+
+height(ans1, ans2);
+
+function control(t) {
+    if (t > '150') {
+        document.write('Ваш ріст підходить.');
+    } else if (t > '210' || t < '150') {
+        document.write('Ваш ріст не підходить.');
+    } else {
+        document.write('Error');
+    };
+};
+
+control(height);
+
+*/
